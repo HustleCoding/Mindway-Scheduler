7e5fdf83e2d018ee04328e0f7e089b25f8c71f57
HustleCoding <f.dobinciuc7@gmail.com>
2023-03-17 10:56:18 +0200

    Add two components for sign in and sign up

---
+++ components/Auth/SignIn.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { signIn } from "../../firebase/auth";
+import { useRedirectAuthenticated } from "../../utils/auth";
+
+const SignIn = () => {
+  useRedirectAuthenticated();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await signIn(email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ components/Auth/SignUp.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { signUp } from "../../firebase/auth";
+import { useRedirectAuthenticated } from "../../utils/auth";
+
+const SignUp = () => {
+  useRedirectAuthenticated();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
